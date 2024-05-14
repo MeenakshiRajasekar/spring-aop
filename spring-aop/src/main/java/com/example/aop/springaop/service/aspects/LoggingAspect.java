400007cf5c6f0637953889ff01e458127844defd
MeenakshiRajasekar <meenakshirajsekar@gmail.com>
2024-05-14 16:17:14 +0530

    updated aspect file

--- spring-aop/src/main/java/com/example/aop/springaop/service/aspects/LoggingAspect.java
+++ spring-aop/src/main/java/com/example/aop/springaop/service/aspects/LoggingAspect.java
@@ -1,18 +1,38 @@
 package com.example.aop.springaop.service.aspects;
 
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import com.example.aop.springaop.service.model.Employee;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
-@EnableAspectJAutoProxy
+@Slf4j
 public class LoggingAspect {
 
-    @Pointcut("executions(* com.example.aop.springaop.controller.*.*(..))")
+    @Pointcut("execution(* com.example.aop.springaop.controller.*.*(..))")
     public void loggingPointCut(){
         System.out.println("logging aspect for employee class");
     }
+
+    @Before("loggingPointCut()")
+    public void before(JoinPoint joinPoint){
+        log.info("Before method invoked: " +joinPoint.getSignature());
+        log.info("Before method args "+ joinPoint.getArgs());
+    }
+
+    @After("loggingPointCut()")
+    public void after(JoinPoint joinPoint){
+
+        log.info("Before method args "+ joinPoint.getArgs());
+    }
+
+    @AfterReturning(value="execution(* com.example.aop.springaop.controller.*.*(..))", returning = "employee")
+    public void afterReturning(JoinPoint joinPoint, Employee employee){
+        log.info("after returning::" +employee);
+    }
+
 }
